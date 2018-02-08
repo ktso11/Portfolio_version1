dacc06209a5c6be4ccd755a915eb00cb63e78632
ktso11 <sokatie11@gmail.com>
2018-02-07 16:34:14 -0800

    completed nav & stats

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("javascript is loaded!")
+$(function(){
+
+$(window).on('scroll', function(){
+  if($(window).scrollTop()>700){
+    var status = $('.picStat div');
+    // if(status.css("width")===10){
+      status.eq(0).animate({width:'25em'}, {duration: 1800});
+      status.eq(1).animate({width:'10em'}, {duration: 1500});
+      status.eq(2).animate({width:'15em'}, {duration: 1200});
+      status.eq(3).animate({width:'25em'}, {duration: 800});
+      status.eq(4).animate({width:'18em'}, {duration: 1200});
+      status.eq(5).animate({width:'100%'}, {duration: 5200});
+    // }
+  }
+})
+
+
+})
